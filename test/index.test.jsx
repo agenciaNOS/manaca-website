c501d6f1c898039264003d503b2994f011bc78ae
MARCELXV <67491786+marcelxv@users.noreply.github.com>
2023-03-05 20:43:40 -0300

    test: add basic test coverage

---
+++ test/index.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import Index from '../pages/index'
+import '@testing-library/jest-dom'
+
+describe('Index', () => {
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Index />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
