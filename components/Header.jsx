1c24330eecbfd0d8f176e743e6ccc23172515e7c
Nicolas Lima <nicolas.lima@redealumni.com>
2023-04-06 01:25:37 -0300

    feat: added DrawerMenu to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
 import { FaInstagram } from 'react-icons/fa';
-import Image from 'next/image' 
+import DrawerMenu from './DrawerMenu';
+import Image from 'next/image'
+import useDevice from '../hooks/use-device';
 
 export default function Header() {
+  const device = useDevice();
+
+  const isMobile = () => device === 'mobile';
+
   return (
-    <>
-      <nav className='flex items-center flex-wrap p-1 justify-between bg-manaca top-0 z-50'>
-          <a className='inline-flex items-center'>
-            <Image src="/manaca-logo-white.svg"
-            alt="Manacá"
-            width={180}
-            height="100%"
-            objectFit="cover"
-            />
-            </a>
-            <a href="https://www.instagram.com/manaca.consultoria" target="_blank" rel="noreferrer" className='inline-flex items-center px-3 py-1 mr-4 bg-white rounded-xl text-manaca'>
-            <span>
-                Instagram
-            </span>
-            <FaInstagram className='ml-2 text-manaca' />
-            </a>
-      </nav>
-    </> 
+    <nav className='flex items-center flex-wrap p-1 justify-between bg-manaca top-0 z-50'>
+      <a className='inline-flex items-center'>
+        <Image src="/manaca-logo-white.svg"
+          alt="Manacá"
+          width={180}
+          height="100%"
+          objectFit="cover"
+        />
+      </a>
+
+      {isMobile() && <DrawerMenu />}
+    </nav>
   )
-    
 }
