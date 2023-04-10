6c087d71e4c7341707574cbbf87a30bb813a5eaf
Nicolas Lima <nicolas.lima@redealumni.com>
2023-04-10 00:33:48 -0300

    style: add color variables to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -8,6 +10,8 @@ module.exports = {
       manaca: '#34824E',
       white: '#FFFFFF',
       black: '#000000',
+      blue: colors.blue,
+      red: colors.red,
       secondary: '#5b145c',
     },
     extend: {},
