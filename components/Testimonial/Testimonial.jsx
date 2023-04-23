d4597d9f84f450a70f8d10f1d8031527dc75b819
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2023-04-23 10:18:03 -0300

    style: fix carousel responsivity

--- components/Testimonial/Testimonial.jsx
+++ components/Testimonial/Testimonial.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { RatingStars } from '../RatingStars';
 
+import useDevice from '../../hooks/use-device';
+
 export default function Testimonial({ description, name, pictureUrl, plusDescription = false, rating, timeAgo }) {
+  const device = useDevice();
+
+  const isDesktop = () => device === 'desktop';
+
   return (
-    <div className="mt-6 mb-6 p-6 rounded sm:min-w-[70%] sm:max-w-[70%] min-w-[50%] max-w-[50%] shadow-xl hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 cursor-pointer">
-      <div className='flex items-center justify-between mb-4'>
-        <div className='flex items-center'>
+    <div
+      className="my-6 lg:p-6 rounded md:min-w-[100%] md:max-w-[100%] lg:min-w-[50%] lg:max-w-[50%] shadow-xl hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 cursor-pointer"
+      style={ !isDesktop() ? { padding: '12px' } : {} }
+    >
+      <div className='flex flex-wrap items-center justify-between mb-4 gap-2'>
+        <div className='flex items-center grow-0'>
           <img src={pictureUrl} referrerPolicy="no-referrer" />
 
-          <span className="ml-4 text-2xl font-bold">
+          <span className="ml-4 lg:text-2xl sm:text-xl md:text-xl font-bold text-clip">
             {name}
           </span>
         </div>
 
-        <RatingStars quantity={rating} />
+        <RatingStars className="grow" quantity={rating} />
       </div>
 
       {Boolean(plusDescription) &&
